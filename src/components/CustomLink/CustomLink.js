0cd8b652a51c4879c42a0642f262e8695c5b32dd
al-amin <arifurr231@gmail.com>
2022-04-05 16:39:13 +0600

    project done

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function CustomLink({ children, to, ...props }) {
-    let resolved = useResolvedPath(to);
-    let match = useMatch({ path: resolved.pathname, end: true });
-  
-    return (
-      <div>
-        <Link
-          style={{ textDecoration: match ? "underline" : "none"}}
-          to={to}
-          {...props}
-        >
-          {children}
-        </Link>
-        {match && ""}
-      </div>
-    );
-  }
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link style={{ textDecoration: match ? "underline" : "none" }} to={to} {...props}>
+        {children}
+      </Link>
+      {match && ""}
+    </div>
+  );
+}
 
 export default CustomLink;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import MyAriaChart from '../MyAriaChart/MyAriaChart';
-import MyBarChart from '../MyBarChart/MyBarChart';
-import MyLineChart from '../MyLineChart/MyLineChart';
-import MyPieChart from '../MyPieChart/MyPieChart';
-import './Dashboard.css';
-
+import React from "react";
+import MyAriaChart from "../MyAriaChart/MyAriaChart";
+import MyBarChart from "../MyBarChart/MyBarChart";
+import MyLineChart from "../MyLineChart/MyLineChart";
+import MyPieChart from "../MyPieChart/MyPieChart";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    return (
-        <div className='charts'>
-            <div>
-            <MyLineChart></MyLineChart>
-            <MyAriaChart></MyAriaChart>
-            </div>
-            <div>
-            <MyBarChart></MyBarChart>
-            <MyPieChart></MyPieChart>
-          </div>
-       </div>
-    );
+  return (
+    <div className="charts">
+      <div>
+        <MyLineChart></MyLineChart>
+        <MyAriaChart></MyAriaChart>
+      </div>
+      <div>
+        <MyBarChart></MyBarChart>
+        <MyPieChart></MyPieChart>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
